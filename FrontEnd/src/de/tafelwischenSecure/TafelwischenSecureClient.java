4a284b4523d53568d4e5e5189635bbb482717429
feri <feri@HECHLERSPC3.Speedport_W_724V_01011603_07_002>
2020-06-13 19:45:13 +0200

    unnötigen JFrame import entfernt.

--- FrontEnd/src/de/tafelwischenSecure/TafelwischenSecureClient.java
+++ FrontEnd/src/de/tafelwischenSecure/TafelwischenSecureClient.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
